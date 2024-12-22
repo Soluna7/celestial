ab5930c514cfe2604d3e08d15bdc842d7b291dcf
Soluna <julia3.williamson@gmail.com>
2024-12-22 15:01:42 -0600

    Arccos Bandpass and Declination

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::*;
 
-const ORBITAL_PERIOD: f64 = 365.25; //measured in rotations per complete revolution (days)
+const ORBITAL_PERIOD: f64 = 365.25; //measured in rotations per complete revolution (days in year)
 const OBLIQUITY: f64 = 23.439281; //tilt of the planet, between -90 and 90 degrees
 
 fn main() {
@@ -18,6 +18,9 @@ fn arccos_bandpass(mut num: f64) -> f64 {
     return num;
 }
 
-fn declination(obliquity: f64) {
-    todo!();
+fn declination(obliquity: f64, orbital_period: f64, time: f64) -> f64 {
+    //time measured in number of full rotations (days)
+    let ecliptic_longitude = (360.0 * time) / orbital_period;
+    let declination: f64 = obliquity * ecliptic_longitude.sin();
+    return declination;
 }
