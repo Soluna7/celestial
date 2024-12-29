fa09c804b00891199810d1d0b1d52bdbe2426fc9
Soluna7 <julia3.williamson@gmail.com>
2024-12-29 12:31:30 -0600

    Added elevation angle, cleaned up code

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,10 @@ fn main() {
     let _altitude: f64 = 302.0; //altitude from radius (sea level) in meters
     let declination = declination(OBLIQUITY, ORBITAL_PERIOD, time);
     let hour_angle = hour_angle(latitude, declination);
+    let elevation_angle = elevation_angle(latitude, declination);
     println!("DECLINATION = {declination}");
     println!("HOUR ANGLE = {hour_angle}");
+    println!("ELEVATION ANGLE = {elevation_angle}");
 }
 
 fn arcsin_bandpass(mut num: f64) -> f64 {
@@ -29,16 +31,19 @@ fn declination(obliquity: f64, orbital_period: f64, time: f64) -> f64 {
     //time measured in number of full rotations (days)
     let ecliptic_longitude = (360.0 * time) / orbital_period;
     let declination: f64 = obliquity * ecliptic_longitude.to_radians().sin();
-    println!("ECLIPTIC LONGITUDE = {ecliptic_longitude}");
-    let x = ecliptic_longitude.to_radians().sin();
-    println!("ECLIPTIC LONGITUDE SINE = {x}");
     return declination;
 }
 
 fn hour_angle(latitude: f64, declination: f64) -> f64 {
-    let ltan = latitude.to_radians().tan();
-    let dtan = declination.to_radians().tan();
-    println!("LATITUDE TANGENT: {ltan}");
-    println!("DECLINATION TANGENT: {dtan}");
-    return -arcsin_bandpass(ltan * dtan);
+    return -arcsin_bandpass(latitude.to_radians().tan() * declination.to_radians().tan());
+}
+
+fn elevation_angle(latitude: f64, declination: f64) -> f64 {
+    let hour_angle = hour_angle(latitude, declination);
+    let elevation_angle =
+        latitude.to_radians().sin()*declination.to_radians().sin()+
+        latitude.to_radians().cos()*declination.to_radians().cos()*
+        hour_angle.to_radians().cos();
+    let elevation_angle = elevation_angle.asin().to_degrees();
+    return elevation_angle;
 }
