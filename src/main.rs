e638ceeab3b1c3d450cd695e1c3a5a5fbb656dea
Soluna7 <julia3.williamson@gmail.com>
2024-12-29 17:16:15 -0700

    Elevation oscillates daily

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,20 @@ use std::*;
 
 const ORBITAL_PERIOD: f64 = 365.256363004; //length of sidereal year in ephemeris days, measured in rotations per complete revolution (basically days in year)
 const OBLIQUITY: f64 = 23.439281; //tilt of the planet, between -90 and 90 degrees
+const TAU: f64 = 6.2831853071;
 
 fn main() {
     let time: f64 = 357.0; //day in year (mod ORBITAL_PERIOD)
     let latitude: f64 = 33.3532905478; //between -90 and 90
-    let _longitude: f64 = -97.8763568637; //between -180 and 180
+    let longitude: f64 = -97.8763568637; //between -180 and 180
     let _altitude: f64 = 302.0; //altitude from radius (sea level) in meters
     let declination = declination(OBLIQUITY, ORBITAL_PERIOD, time);
     let hour_angle = hour_angle(latitude, declination);
-    let elevation_angle = elevation_angle(latitude, declination);
+    let max_elevation_angle = max_elevation_angle(latitude, declination);
+    let elevation_angle = elevation_angle(OBLIQUITY, ORBITAL_PERIOD, latitude, longitude, time);
     println!("DECLINATION = {declination}");
     println!("HOUR ANGLE = {hour_angle}");
+    println!("MAX ELEVATION ANGLE = {max_elevation_angle}");
     println!("ELEVATION ANGLE = {elevation_angle}");
 }
 
@@ -38,12 +41,22 @@ fn hour_angle(latitude: f64, declination: f64) -> f64 {
     return -arcsin_bandpass(latitude.to_radians().tan() * declination.to_radians().tan());
 }
 
-fn elevation_angle(latitude: f64, declination: f64) -> f64 {
-    let hour_angle = hour_angle(latitude, declination);
-    let elevation_angle =
+fn max_elevation_angle(latitude: f64, declination: f64) -> f64 {
+    let max_elevation_angle =
         latitude.to_radians().sin()*declination.to_radians().sin()+
-        latitude.to_radians().cos()*declination.to_radians().cos()*
-        hour_angle.to_radians().cos();
-    let elevation_angle = elevation_angle.asin().to_degrees();
+        latitude.to_radians().cos()*declination.to_radians().cos();
+    let max_elevation_angle = max_elevation_angle.asin().to_degrees();
+    return max_elevation_angle;
+}
+
+fn elevation_angle(obliquity:f64, orbital_period:f64, latitude:f64, longitude:f64, time:f64) -> f64 {
+    let declination = declination(obliquity, orbital_period, time+longitude/orbital_period);
+    let maximum_elevation_angle = max_elevation_angle(latitude, declination);
+    let minimum_elevation_angle = max_elevation_angle(-latitude, declination);
+    let oscillator = (TAU * (time+(longitude/orbital_period))) % TAU;
+    println!("{oscillator}");
+    let elevation_angle = 
+        (maximum_elevation_angle - minimum_elevation_angle)/2.0 +
+        (maximum_elevation_angle + minimum_elevation_angle)/2.0 * oscillator.sin();
     return elevation_angle;
 }
