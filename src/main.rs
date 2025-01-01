4cd5f240490add82f7d2b075df46b48d9985c3c9
Soluna7 <julia3.williamson@gmail.com>
2025-01-01 12:37:51 -0700

    Complete Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,23 @@
 use std::*;
+use num_traits::sign::signum;
 
 const ORBITAL_PERIOD: f64 = 365.256363004; //length of sidereal year in ephemeris days, measured in rotations per complete revolution (basically days in year)
 const OBLIQUITY: f64 = 23.439281; //tilt of the planet, between -90 and 90 degrees
-const TAU: f64 = 6.2831853071;
 
 fn main() {
-    let time: f64 = 357.0; //day in year (mod ORBITAL_PERIOD)
+    let time: f64 = 357.5; //day in year (mod ORBITAL_PERIOD)
     let latitude: f64 = 33.3532905478; //between -90 and 90
     let longitude: f64 = -97.8763568637; //between -180 and 180
-    let _altitude: f64 = 302.0; //altitude from radius (sea level) in meters
     let declination = declination(OBLIQUITY, ORBITAL_PERIOD, time);
-    let hour_angle = hour_angle(latitude, declination);
-    let max_elevation_angle = max_elevation_angle(latitude, declination);
-    let elevation_angle = elevation_angle(OBLIQUITY, ORBITAL_PERIOD, latitude, longitude, time);
+    let hour_angle = hour_angle(longitude, time);
+    let zenith_angle = zenith_angle(latitude, declination, hour_angle);
+    let elevation_angle = elevation_angle(latitude, declination, hour_angle);
+    let azimuth_angle = azimuth_angle(latitude, declination, hour_angle);
     println!("DECLINATION = {declination}");
     println!("HOUR ANGLE = {hour_angle}");
-    println!("MAX ELEVATION ANGLE = {max_elevation_angle}");
+    println!("ZENITH ANGLE = {zenith_angle}");
     println!("ELEVATION ANGLE = {elevation_angle}");
-}
-
-fn arcsin_bandpass(mut num: f64) -> f64 {
-    if num < -1.0 {
-        num = -90.0;
-    } else if num > 1.0 {
-        num = 90.0;
-    } else {
-        num = num.asin().to_degrees();
-    }
-    return num;
+    println!("AZIMUTH ANGLE = {azimuth_angle}");
 }
 
 fn declination(obliquity: f64, orbital_period: f64, time: f64) -> f64 {
@@ -37,47 +27,39 @@ fn declination(obliquity: f64, orbital_period: f64, time: f64) -> f64 {
     return declination;
 }
 
-fn hour_angle(latitude: f64, declination: f64) -> f64 {
-    return -arcsin_bandpass(latitude.to_radians().tan() * declination.to_radians().tan());
+fn hour_angle(longitude: f64, time: f64) -> f64 {
+    let day_angle: f64 = (360.0*(time%1.0)+longitude)%360.0;
+    return day_angle+180.0*signum(day_angle);
 }
 
-fn max_elevation_angle(latitude: f64, declination: f64) -> f64 {
-    let max_elevation_angle =
-        latitude.to_radians().sin()*declination.to_radians().sin()+
-        latitude.to_radians().cos()*declination.to_radians().cos();
-    let max_elevation_angle = max_elevation_angle.asin().to_degrees();
-    return max_elevation_angle;
+fn zenith_angle(latitude:f64, declination:f64, hour_angle: f64) -> f64 {
+    let zenith_angle:f64 = 
+        latitude.to_radians().sin()*declination.to_radians().sin() +
+        latitude.to_radians().cos()*declination.to_radians().cos()*hour_angle.to_radians().cos();
+    let zenith_angle = zenith_angle.acos().to_degrees();
+    return zenith_angle;
 }
 
-fn elevation_angle(obliquity:f64, orbital_period:f64, latitude:f64, longitude:f64, time:f64) -> f64 {
-    let declination = declination(obliquity, orbital_period, time+longitude/orbital_period);
-    let maximum_elevation_angle = max_elevation_angle(latitude, declination);
-    let minimum_elevation_angle = max_elevation_angle(-latitude, declination);
-    let oscillator = (TAU * (time+(longitude/orbital_period))) % TAU;
-    let elevation_angle = 
-        (maximum_elevation_angle - minimum_elevation_angle)/2.0 +
-        (maximum_elevation_angle + minimum_elevation_angle)/2.0 * oscillator.sin();
+fn elevation_angle(latitude:f64, declination:f64, hour_angle:f64) -> f64 {
+    let elevation_angle: f64 = 90.0 - zenith_angle(latitude, declination, hour_angle);
     return elevation_angle;
 }
 
-fn max_azimuth_angle(latitude:f64, declination:f64) -> f64 {
-    let max_azimuth_angle = (
-        latitude.to_radians().cos()*declination.to_radians().sin() - 
-        latitude.to_radians().sin()*declination.to_radians().cos() *
-        hour_angle(latitude, declination)
-    )/max_elevation_angle(latitude, declination);
-    let max_azimuth_angle = max_azimuth_angle.acos().to_degrees();
-    return max_azimuth_angle;
-}
-
-fn azimuth_angle(obliquity:f64, orbital_period:f64, latitude:f64, longitude:f64, time:f64) -> f64 {
-    let declination = declination(obliquity, orbital_period, time + longitude/orbital_period);
-    let maximum_azimuth_angle = max_azimuth_angle(latitude, declination);
-    let minimum_azimuth_angle = max_azimuth_angle(-latitude, declination);
-    let oscillator = (TAU * (time+(longitude/orbital_period))) % TAU;
-    let azimuth_angle = 
-        (maximum_azimuth_angle - minimum_azimuth_angle)/2.0 +
-        (maximum_azimuth_angle + minimum_azimuth_angle)/2.0 * oscillator.sin();
-    return azimuth_angle;
-    //hour angle?
+fn azimuth_angle(latitude:f64, declination:f64, hour_angle:f64) -> f64 {
+    let zenith_angle = zenith_angle(latitude, declination, hour_angle);
+    if hour_angle > 0.0 {
+        let azimuth_angle = (
+            latitude.to_radians().sin()*zenith_angle.to_radians().cos() - 
+            declination.to_radians().sin() 
+            ) / latitude.to_radians().cos()*declination.to_radians().sin();
+        let azimuth_angle = (azimuth_angle.acos().to_degrees() + 180.0) % 360.0;
+        return azimuth_angle;
+    } else {
+        let azimuth_angle = (
+            latitude.to_radians().sin()*zenith_angle.to_radians().cos() - 
+            zenith_angle.to_radians().sin()
+            ) / latitude.to_radians().cos()*zenith_angle.to_radians().sin();
+        let azimuth_angle = (540.0 - azimuth_angle.acos().to_degrees()) % 360.0;
+        return azimuth_angle;
+    }
 }
