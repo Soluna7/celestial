d99ab419086a13cf6021c36bb446ea665a6b6eeb
Soluna7 <julia3.williamson@gmail.com>
2025-01-01 13:27:01 -0700

    Changed modular operations to euclid remainders

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,23 @@ const ORBITAL_PERIOD: f64 = 365.256363004; //length of sidereal year in ephemeri
 const OBLIQUITY: f64 = 23.439281; //tilt of the planet, between -90 and 90 degrees
 
 fn main() {
-    let time: f64 = 357.5; //day in year (mod ORBITAL_PERIOD)
-    let latitude: f64 = 33.3532905478; //between -90 and 90
-    let longitude: f64 = -97.8763568637; //between -180 and 180
-    let declination = declination(OBLIQUITY, ORBITAL_PERIOD, time);
-    let hour_angle = hour_angle(longitude, time);
-    let zenith_angle = zenith_angle(latitude, declination, hour_angle);
-    let elevation_angle = elevation_angle(latitude, declination, hour_angle);
-    let azimuth_angle = azimuth_angle(latitude, declination, hour_angle);
-    println!("DECLINATION = {declination}");
-    println!("HOUR ANGLE = {hour_angle}");
-    println!("ZENITH ANGLE = {zenith_angle}");
-    println!("ELEVATION ANGLE = {elevation_angle}");
-    println!("AZIMUTH ANGLE = {azimuth_angle}");
+    let mut time: f64 = 0.0; //day in year (mod ORBITAL_PERIOD)
+    while time <= 2.0 {
+        let latitude: f64 = 33.3532905478; //between -90 and 90
+        let longitude: f64 = -97.8763568637; //between -180 and 180
+        let declination = declination(OBLIQUITY, ORBITAL_PERIOD, time);
+        let hour_angle = hour_angle(longitude, time);
+        let zenith_angle = zenith_angle(latitude, declination, hour_angle);
+        let elevation_angle = elevation_angle(latitude, declination, hour_angle);
+        let azimuth_angle = azimuth_angle(latitude, declination, hour_angle);
+        println!("TIME = {time}");
+        //println!("DECLINATION = {declination}");
+        //println!("HOUR ANGLE = {hour_angle}");
+        //println!("ZENITH ANGLE = {zenith_angle}");
+        //println!("ELEVATION ANGLE = {elevation_angle}");
+        println!("AZIMUTH ANGLE = {azimuth_angle}");
+        time = time + 0.01;
+    }
 }
 
 fn declination(obliquity: f64, orbital_period: f64, time: f64) -> f64 {
@@ -27,7 +31,8 @@ fn declination(obliquity: f64, orbital_period: f64, time: f64) -> f64 {
 }
 
 fn hour_angle(longitude: f64, time: f64) -> f64 {
-    let day_angle: f64 = (360.0*(time%1.0)+longitude)%360.0;
+    let day_angle: f64 = (360.0*(time%1.0)+longitude);
+    let day_angle = day_angle.rem_euclid(360.0);
     return day_angle+180.0*signum(day_angle);
 }
 
@@ -51,14 +56,20 @@ fn azimuth_angle(latitude:f64, declination:f64, hour_angle:f64) -> f64 {
             latitude.to_radians().sin()*zenith_angle.to_radians().cos() - 
             declination.to_radians().sin() 
             ) / latitude.to_radians().cos()*declination.to_radians().sin();
-        let azimuth_angle = (azimuth_angle.acos().to_degrees() + 180.0) % 360.0;
+            println!("{azimuth_angle}");
+        let azimuth_angle = ((azimuth_angle+1.0).rem_euclid(2.0))-1.0;
+        let azimuth_angle = (azimuth_angle.acos().to_degrees() + 180.0);
+        let azimuth_angle = azimuth_angle.rem_euclid(360.0);
         return azimuth_angle;
     } else {
         let azimuth_angle = (
             latitude.to_radians().sin()*zenith_angle.to_radians().cos() - 
             zenith_angle.to_radians().sin()
             ) / latitude.to_radians().cos()*zenith_angle.to_radians().sin();
-        let azimuth_angle = (540.0 - azimuth_angle.acos().to_degrees()) % 360.0;
+            println!("{azimuth_angle}");
+        let azimuth_angle = ((azimuth_angle+1.0).rem_euclid(2.0))-1.0;
+        let azimuth_angle = (540.0 - azimuth_angle.acos().to_degrees());
+        let azimuth_angle = azimuth_angle.rem_euclid(360.0);
         return azimuth_angle;
     }
 }
