5747a9fb5b398ffd1f40ce964d274a948a4c044c
Soluna7 <julia3.williamson@gmail.com>
2025-01-01 15:42:03 -0700

    Removed unnececary parentheses

--- src/functions.rs
+++ src/functions.rs
@@ -8,7 +8,7 @@ pub fn declination(obliquity: f64, orbital_period: f64, time: f64) -> f64 {
 }
 
 pub fn hour_angle(longitude: f64, time: f64) -> f64 {
-    let day_angle: f64 = (360.0*(time%1.0)+longitude);
+    let day_angle: f64 = 360.0*(time%1.0)+longitude;
     let day_angle = day_angle.rem_euclid(360.0);
     return day_angle+180.0*signum(day_angle);
 }
@@ -35,7 +35,7 @@ pub fn azimuth_angle(latitude:f64, declination:f64, hour_angle:f64) -> f64 {
             ) / latitude.to_radians().cos()*declination.to_radians().sin();
             println!("{azimuth_angle}");
         let azimuth_angle = ((azimuth_angle+1.0).rem_euclid(2.0))-1.0;
-        let azimuth_angle = (azimuth_angle.acos().to_degrees() + 180.0);
+        let azimuth_angle = azimuth_angle.acos().to_degrees() + 180.0;
         let azimuth_angle = azimuth_angle.rem_euclid(360.0);
         return azimuth_angle;
     } else {
@@ -45,7 +45,7 @@ pub fn azimuth_angle(latitude:f64, declination:f64, hour_angle:f64) -> f64 {
             ) / latitude.to_radians().cos()*zenith_angle.to_radians().sin();
             println!("{azimuth_angle}");
         let azimuth_angle = ((azimuth_angle+1.0).rem_euclid(2.0))-1.0;
-        let azimuth_angle = (540.0 - azimuth_angle.acos().to_degrees());
+        let azimuth_angle = 540.0 - azimuth_angle.acos().to_degrees();
         let azimuth_angle = azimuth_angle.rem_euclid(360.0);
         return azimuth_angle;
     }
