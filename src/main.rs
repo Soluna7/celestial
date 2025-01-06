025744ec0ed5d920271dcf52b2f7c8fe337d1b89
Soluna <julia3.williamson@gmail.com>
2025-01-06 17:59:00 -0600

    Refactor and Repair

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 mod solar;
 
+use crate::solar::Polar;
+
 const ORBITAL_PERIOD: f64 = 365.256363004; //length of sidereal year in ephemeris days, measured in rotations per complete revolution (basically days in year)
 const OBLIQUITY: f64 = 23.439281; //tilt of the planet, between -90 and 90 degrees
 
 fn main() {
-    let time: f64 = 0.0; //day in year (mod ORBITAL_PERIOD)
-    let latitude: f64 = -90.0; //between -90 and 90
-    let longitude: f64 = -180.0; //between -180 and 180
-    let sun_position = solar::sun_position(OBLIQUITY, ORBITAL_PERIOD, latitude, longitude, time);
-    println!("{:?}", sun_position);
+    let mut time: f64 = 0.0;
+    while time < 1.0 {
+        let player_position = Polar {
+            longitude: 0.0,
+            altitude: 0.0,
+            latitude: 30.0,
+        };
+        let sun_position = solar::sun_position(OBLIQUITY, ORBITAL_PERIOD, player_position, time);
+        let solar_azimuth = sun_position.azimuth;
+        let solar_elevation = sun_position.elevation;
+        println!("TIME: {time} | AZIMUTH: {solar_azimuth} | ELEVATION: {solar_elevation}");
+        time += 1.0 / 24.0;
+    }
 }
--- src/solar.rs
+++ src/solar.rs
@@ -1,9 +1,17 @@
-use num_traits::sign::signum;
+pub struct Polar {
+    pub longitude: f64,
+    pub altitude: f64,
+    pub latitude: f64,
+}
 
-#[derive(Debug)]
-pub struct CelestialPosition {
-    elevation: f64,
-    azimuth: f64,
+pub struct Celestial {
+    pub azimuth: f64,
+    pub elevation: f64,
+}
+
+fn hour_angle(time: f64) -> f64 {
+    let hour_angle = 360.0 * (time % 1.0);
+    return hour_angle - hour_angle.signum() * 180.0;
 }
 
 fn declination(obliquity: f64, orbital_period: f64, time: f64) -> f64 {
@@ -13,59 +21,43 @@ fn declination(obliquity: f64, orbital_period: f64, time: f64) -> f64 {
     return declination;
 }
 
-fn hour_angle(longitude: f64, time: f64) -> f64 {
-    let day_angle: f64 = 360.0*(time%1.0)+longitude;
-    let day_angle = day_angle.rem_euclid(360.0);
-    return day_angle+180.0*signum(day_angle);
+fn elevation_angle(latitude: f64, declination: f64, time: f64) -> f64 {
+    let hour_angle = hour_angle(time);
+    let elevation_angle = latitude.to_radians().sin() * declination.to_radians().sin()
+        + latitude.to_radians().cos()
+            * declination.to_radians().cos()
+            * hour_angle.to_radians().cos();
+    return elevation_angle.asin().to_degrees();
 }
 
-fn zenith_angle(latitude:f64, declination:f64, hour_angle: f64) -> f64 {
-    let zenith_angle:f64 = 
-        latitude.to_radians().sin()*declination.to_radians().sin() +
-        latitude.to_radians().cos()*declination.to_radians().cos()*hour_angle.to_radians().cos();
-    let zenith_angle = zenith_angle.acos().to_degrees();
-    return zenith_angle;
-}
-
-fn elevation_angle(latitude:f64, declination:f64, hour_angle:f64) -> f64 {
-    let elevation_angle: f64 = 90.0 - zenith_angle(latitude, declination, hour_angle);
-    return elevation_angle;
-}
-
-fn azimuth_angle(latitude:f64, declination:f64, hour_angle:f64) -> f64 {
-    let zenith_angle = zenith_angle(latitude, declination, hour_angle);
-    if hour_angle > 0.0 {
-        let azimuth_angle = (
-            latitude.to_radians().sin()*zenith_angle.to_radians().cos() - 
-            declination.to_radians().sin() 
-            ) / latitude.to_radians().cos()*declination.to_radians().sin();
-            println!("{azimuth_angle}");
-        let azimuth_angle = ((azimuth_angle+1.0).rem_euclid(2.0))-1.0;
-        let azimuth_angle = azimuth_angle.acos().to_degrees() + 180.0;
-        let azimuth_angle = azimuth_angle.rem_euclid(360.0);
-        return azimuth_angle;
+fn azimuth_angle(latitude: f64, declination: f64, time: f64) -> f64 {
+    if latitude.abs() >= 90.0 {
+        return 360.0 * (-latitude.signum() * time + (latitude.signum() + 1.0) / 4.0) % 1.0;
+    }
+    let elevation_angle = elevation_angle(latitude, declination, time);
+    let azimuth_angle = (latitude.to_radians().sin() * elevation_angle.to_radians().sin()
+        - declination.to_radians().sin())
+        / (latitude.to_radians().cos() * elevation_angle.to_radians().cos());
+    let azimuth_angle = azimuth_angle.acos().to_degrees();
+    if hour_angle(time) > 0.0 {
+        let azimuth_angle = azimuth_angle + 180.0;
+        return azimuth_angle % 360.0;
     } else {
-        let azimuth_angle = (
-            latitude.to_radians().sin()*zenith_angle.to_radians().cos() - 
-            zenith_angle.to_radians().sin()
-            ) / latitude.to_radians().cos()*zenith_angle.to_radians().sin();
-            println!("{azimuth_angle}");
-        let azimuth_angle = ((azimuth_angle+1.0).rem_euclid(2.0))-1.0;
-        let azimuth_angle = 540.0 - azimuth_angle.acos().to_degrees();
-        let azimuth_angle = azimuth_angle.rem_euclid(360.0);
-        return azimuth_angle;
+        let azimuth_angle = 540.0 - azimuth_angle;
+        return azimuth_angle % 360.0;
     }
 }
 
-pub fn sun_position(obliquity: f64, orbital_period: f64, latitude:f64, longitude: f64, time: f64) -> CelestialPosition {
+pub fn sun_position(obliquity: f64, orbital_period: f64, position: Polar, time: f64) -> Celestial {
+    let longitude = position.longitude;
+    let latitude = position.latitude;
+    let time = time + longitude / 180.0;
     let declination = declination(obliquity, orbital_period, time);
-    let hour_angle = hour_angle(longitude, time);
-    let elevation_angle = elevation_angle(latitude, declination, hour_angle);
-    let azimuth_angle = azimuth_angle(latitude, declination, hour_angle);
-    let sun_position = CelestialPosition{
-        elevation: elevation_angle, 
-        azimuth: azimuth_angle
+    let elevation_angle = elevation_angle(latitude, declination, time);
+    let azimuth_angle = azimuth_angle(latitude, declination, time);
+    let sun_position = Celestial {
+        azimuth: azimuth_angle,
+        elevation: elevation_angle,
     };
     return sun_position;
 }
-
